29634f09a53c6b70b79b0e5c14281f311d04f700
Atticus White <contact@atticuswhite.com>
2017-09-17 17:44:25 -0400

    web app

---
+++ web-app/pages/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/nav'
+
+export default () => (
+  <div>
+    <Head title="Home" />
+    <img
+      src="http://apartment:43973"
+      style={{minWidth: 400, minHeight: 400, maxWidth: 600, maxHeight: 500}}
+    />
+  </div>
+)
